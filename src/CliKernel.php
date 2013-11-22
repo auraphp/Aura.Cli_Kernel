7c0c7a038080d8d2b2ef7343b67b5efa96ad37e4
Paul M. Jones <pmjones88@gmail.com>
2013-11-21 20:37:41 -0600

    need to pass params differently

--- src/CliKernel.php
+++ src/CliKernel.php
@@ -15,24 +15,34 @@ class CliKernel extends ProjectKernel
         $stdio      = $this->di->get('cli_stdio');
         $dispatcher = $this->di->get('cli_dispatcher');
         
-        // get the command name
-        $name = $context->argv->shift();
+        // get the params for the dispatcher
+        $params = $context->argv->get();
+        
+        // strip the console script name
+        array_shift($params);
+        
+        // strip the command name, and replace as a named param
+        $command = array_shift($params);
         
         // is there a command name specified?
-        if (! $name) {
+        if (! $command) {
             $stdio->errln('No command specified.');
             return Status::USAGE;
         }
         
         // does the command exist?
-        if (! $dispatcher->hasObject($name)) {
-            $stdio->errln("Command '$name' not recognized.");
+        if (! $dispatcher->hasObject($command)) {
+            $stdio->errln("Command '{$command}' not recognized.");
             return Status::UNAVAILABLE;
         }
         
+        // place the command back as a named param; the rest remain as
+        // sequential params.
+        $params['command'] = $command;
+        
         // dispatch to the command
         try {
-            $result = $dispatcher->__invoke($name);
+            $result = $dispatcher->__invoke($params);
             return (int) $result;
         } catch (Exception $e) {
             $stdio->errln($e->getMessage());
