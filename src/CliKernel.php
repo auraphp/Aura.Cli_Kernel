af949598ab5bc0b815a806f00216fa89bfda19ef
Paul M. Jones <pmjones88@gmail.com>
2013-11-24 22:20:27 -0600

    update comments

--- src/CliKernel.php
+++ src/CliKernel.php
@@ -1,12 +1,35 @@
 <?php
+/**
+ * 
+ * This file is part of Aura for PHP.
+ * 
+ * @package Aura.Cli_Kernel
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ */
 namespace Aura\Cli_Kernel;
 
 use Aura\Cli\Status;
 use Aura\Project_Kernel\ProjectKernel;
 use Exception;
 
+/**
+ * 
+ * A kernel for Aura CLI projects.
+ * 
+ * @package Aura.Cli_Kernel
+ * 
+ */
 class CliKernel extends ProjectKernel
 {
+    /**
+     * 
+     * Invokes the kernel (i.e., runs it).
+     * 
+     * @return int The exit code.
+     * 
+     */
     public function __invoke()
     {
         parent::__invoke();
