e778d0a51e4762bb45f526cdca2e2c183a62c9e0
Paul M. Jones <pmjones88@gmail.com>
2013-12-17 16:24:47 -0600

    no more cli_kernel service, use a new instance instead

--- config/default/define.php
+++ config/default/define.php
@@ -17,7 +17,6 @@
 $di->set('cli_context', $di->lazyNew('Aura\Cli\Context'));
 $di->set('cli_stdio', $di->lazyNew('Aura\Cli\Stdio'));
 $di->set('cli_dispatcher', $di->lazyNew('Aura\Dispatcher\Dispatcher'));
-$di->set('cli_kernel', $di->lazyNew('Aura\Cli_Kernel\CliKernel'));
 
 /**
  * Aura\Cli_Kernel\CliKernel
