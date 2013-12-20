ac3ab0da7d0644505d36a05d32bbe8fa3581ad78
Paul M. Jones <pmjones88@gmail.com>
2013-12-19 19:30:57 -0600

    turn off logging per @harikt

--- config/integration/modify.php
+++ config/integration/modify.php
@@ -1,7 +1,10 @@
 <?php
+$logger = $di->get('logger');
 $dispatcher = $di->get('cli_dispatcher');
 $context = $di->get('cli_context');
 $stdio   = $di->get('cli_stdio');
+
+$logger->pushHandler($di->newInstance('Monolog\Handler\NullHandler'));
 
 $dispatcher->setObject('aura-integration-hello', function () use ($stdio) {
     $stdio->outln("Hello World!");
