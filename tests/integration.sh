e76f0daeb5cfe32409e510d9c704d06f70f8d922
Paul M. Jones <pmjones88@gmail.com>
2014-01-31 17:02:56 -0600

    no more scripts dir

--- tests/integration.sh
+++ tests/integration.sh
@@ -16,7 +16,6 @@ cp -r autoload.php  cli-project/vendor/aura/cli-kernel/
 cp -r composer.json cli-project/vendor/aura/cli-kernel/
 cp -r config        cli-project/vendor/aura/cli-kernel/
 cp -r README.md     cli-project/vendor/aura/cli-kernel/
-cp -r scripts       cli-project/vendor/aura/cli-kernel/
 cp -r src           cli-project/vendor/aura/cli-kernel/
 cp -r tests         cli-project/vendor/aura/cli-kernel/
 cd cli-project/vendor/aura/cli-kernel/tests
