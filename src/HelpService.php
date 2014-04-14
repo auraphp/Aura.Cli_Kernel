787b0423f29ec4394c8ac1501b98151f9c1d49f0
Paul M. Jones <pmjones88@gmail.com>
2014-04-14 16:04:04 -0500

    add docs

--- src/HelpService.php
+++ src/HelpService.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * 
+ * This file is part of Aura for PHP.
+ * 
+ * @package Aura.Cli_Kernel
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ */
 namespace Aura\Cli_Kernel;
 
 /**
