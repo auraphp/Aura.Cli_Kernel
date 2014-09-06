92b514c02b9c63e5fe42e29f4822236107acff73
Paul M. Jones <pmjones88@gmail.com>
2014-09-06 11:28:49 -0500

    modify testing structure

--- tests/kernel/src/CliKernelTest.php
+++ tests/kernel/src/CliKernelTest.php
@@ -15,7 +15,7 @@ class CliKernelTest extends \PHPUnit_Framework_TestCase
         array_unshift($argv, 'cli/console.php');
         $_SERVER['argv'] = $argv;
 
-        $path = __DIR__;
+        $path = dirname(__DIR__);
         $di = (new Factory)->newContainer(
             $path,
             'kernel',
